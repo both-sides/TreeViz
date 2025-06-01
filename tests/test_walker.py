6c23da3a6bbff4eed5ac2cad2b8e3c9817025737
Michael <ijohnkojo@gmail.com>
2025-05-31 20:29:14 -0500

    still figuring out why pytest can't find treeviz.walker

--- tests/test_walker.py
+++ tests/test_walker.py
@@ -1,8 +1,9 @@
 import pytest
+import treeviz.walker
 from treeviz.walker import walk
 
-    def test_walk_empty(tmp_path):
-        root = tmp_path / "empty"
-        root.mkdir()
-        nodes = list(walk(str(root))) #forces walk generator to run till completion
-        assert len(nodes) >= 1, "there should be at least one node"
+def test_walk_empty(tmp_path):
+    root = tmp_path / "empty"
+    root.mkdir()
+    nodes = list(walk(str(root))) #forces walk generator to run till completion
+    assert len(nodes) >= 1, "there should be at least one node"
