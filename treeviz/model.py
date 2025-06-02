3857496003d61a29440665e3bab7fa323d73f4f7
Michael <ijohnkojo@gmail.com>
2025-06-01 22:37:45 -0500

    edit in some files

--- treeviz/model.py
+++ treeviz/model.py
@@ -1,9 +1,13 @@
+#A simple tree node representing a single file or directory
+
+
+
 class Node:
     def __init__(self, name, path, children = None):
-        self.name = name
-        self.path = path
-        self.children = children or []
-        self.expanded = False
+        self.name = name #basename (string)
+        self.path = path #full path to this node (string)
+        self.children = children or [] #list of child node objects (will be populated by walker)
+        self.expanded = False # reserved for TUI
 
     def __repr__(self):
         return f"<Node {self.path!r}>"
--- treeviz/walker.py
+++ treeviz/walker.py
@@ -1,7 +1,7 @@
 import os
 from .model import Node
-#from .config import DEFAULTS
+from .config import DEFAULTS
 
-def walk(root_path, max_depth = 5, follow_symlinks = False):
+def walk(root_path, max_depth = DEFAULTS["max_depth"], follow_symlinks = False):
     #TODO: implement recursive scan with cycle detection
     yield Node(name = os.path.basename(root_path), path = root_path)
