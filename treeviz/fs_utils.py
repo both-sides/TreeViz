973d3fc59ce5b134213e32105ebea08cedb8d13f
Michael <ijohnkojo@gmail.com>
2025-06-03 22:26:28 -0500

    still working on fs_utils.py

--- treeviz/fs_utils.py
+++ treeviz/fs_utils.py
@@ -1,4 +1,18 @@
+#I'm trying to catch permission errors or file not found when trying to list a directory.
+
 import os 
 import stat
 
-class U
+class UnreadableDirectory(Exception):
+
+def safe_scandir(path):
+    try:
+        with os.scandir(path) as it: #should close the resource(ScandirIterator) if errors are caught
+            for entry in it:
+                yield entry
+    except(PermissionError, FileNotFoundError, OSError) as e:
+        raise UnreadableDirectory(str(e)) # should wrap this custom exception so that walker can catch it
+
+    def is_dir(entry, follow_syslinks): # should return true if entry is a directory , and should honor syslink flag
+        try:
+
